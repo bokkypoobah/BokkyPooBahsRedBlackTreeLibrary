8ea8b6246af2f20d3f9bbb6f0dc917d2575f3a89
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2018-10-16 18:14:35 +1100

    Getting ready for optimisation

--- test/01_test1.sh
+++ test/01_test1.sh
@@ -110,8 +110,7 @@ for (var i = 1; i <= NUMBEROFITEMS; i++) {
     items.push(i);
 }
 // items = shuffle(items);
-// Following broke it
-// items=[11,35,22,70,57,54,49,58,33,74,46,41,68,16,10,34,31,96,43,30,98,79,55,47,77,7,72,86,89,64,83,14,38,81,100,78,12,36,62,99,84,92,60,32,53,24,97,4,87,26,93,25,56,63,5,67,51,76,59,66,69,65,48,39,18,3,45,50,8,71,95,19,28,52,82,1,20,6,75,27,9,88,23,17,42,85,44,13,80,37,94,40,2,21,15,90,61,91,73,29];
+items=[11,35,22,70,57,54,49,58,33,74,46,41,68,16,10,34,31,96,43,30,98,79,55,47,77,7,72,86,89,64,83,14,38,81,100,78,12,36,62,99,84,92,60,32,53,24,97,4,87,26,93,25,56,63,5,67,51,76,59,66,69,65,48,39,18,3,45,50,8,71,95,19,28,52,82,1,20,6,75,27,9,88,23,17,42,85,44,13,80,37,94,40,2,21,15,90,61,91,73,29];
 // items = [15,14,20,3,7,10,11,16,18,2,4,5,8,19,1,9,12,6,17,13];
 // items = [4, 3, 1, 2, 6, 7, 5, 8, 9];
 // items = [4, 3, 1, 2, 6];
@@ -141,8 +140,8 @@ for (var i = 0; i < items.length; i++) {
   totalGasUsed = totalGasUsed.add(eth.getTransactionReceipt(tx[i]).gasUsed);
 }
 var averageGasUsed = totalGasUsed.div(items.length);
-console.log("RESULT: totalGasUsed=" + totalGasUsed);
-console.log("RESULT: averageGasUsed=" + averageGasUsed);
+console.log("RESULT: totalGasUsedInsert=" + totalGasUsed);
+console.log("RESULT: averageGasUsedInsert=" + averageGasUsed);
 console.log("RESULT: ");
 
 printTestRedBlackTreeContractDetails();
@@ -150,8 +149,7 @@ console.log("RESULT: ");
 
 
 items = shuffle(items);
-// Following broke it
-// items=[97,1,26,99,32,56,21,83,42,88,49,8,69,9,78,18,20,17,39,87,25,7,5,91,73,15,52,80,96,61,16,29,55,76,48,37,44,14,98,31,34,85,28,4,72,27,13,43,62,38,40,92,24,60,41,50,22,45,57,70,2,82,75,81,35,74,58,59,10,36,100,68,23,67,11,86,77,90,12,93,54,47,79,63,51,6,33,84,94,19,65,46,30,3,71,95,89,64,53,66];
+items=[97,1,26,99,32,56,21,83,42,88,49,8,69,9,78,18,20,17,39,87,25,7,5,91,73,15,52,80,96,61,16,29,55,76,48,37,44,14,98,31,34,85,28,4,72,27,13,43,62,38,40,92,24,60,41,50,22,45,57,70,2,82,75,81,35,74,58,59,10,36,100,68,23,67,11,86,77,90,12,93,54,47,79,63,51,6,33,84,94,19,65,46,30,3,71,95,89,64,53,66];
 // items = [15,14,20,3,7,10,11,16,18,2,4,5,8,19,1,9,12,6,17,13];
 // items = [4, 3, 1, 2, 6, 7, 5, 8, 9];
 // items = [4, 3, 1, 2, 6];
@@ -193,8 +191,8 @@ for (var i = 0; i < items.length /*&& i < 75*/; i++) {
   totalGasUsed = totalGasUsed.add(eth.getTransactionReceipt(tx[i]).gasUsed);
 }
 var averageGasUsed = totalGasUsed.div(items.length);
-console.log("RESULT: totalGasUsed=" + totalGasUsed);
-console.log("RESULT: averageGasUsed=" + averageGasUsed);
+console.log("RESULT: totalGasUsedRemove=" + totalGasUsed);
+console.log("RESULT: averageGasUsedRemove=" + averageGasUsed);
 console.log("RESULT: ");
 
 exit;
@@ -261,8 +259,8 @@ for (var i = 0; i < items.length; i++) {
   totalGasUsed = totalGasUsed.add(eth.getTransactionReceipt(tx[i]).gasUsed);
 }
 var averageGasUsed = totalGasUsed.div(items.length);
-console.log("RESULT: totalGasUsed=" + totalGasUsed);
-console.log("RESULT: averageGasUsed=" + averageGasUsed);
+console.log("RESULT: totalGasUsedRemove=" + totalGasUsed);
+console.log("RESULT: averageGasUsedRemove=" + averageGasUsed);
 
 console.log("RESULT: ");
 
@@ -274,5 +272,4 @@ grep "DATA: " $TEST1OUTPUT | sed "s/DATA: //" > $DEPLOYMENTDATA
 cat $DEPLOYMENTDATA
 grep "RESULT: " $TEST1OUTPUT | sed "s/RESULT: //" > $TEST1RESULTS
 cat $TEST1RESULTS
-# grep "JSONSUMMARY: " $TEST1OUTPUT | sed "s/JSONSUMMARY: //" > $JSONSUMMARY
-# cat $JSONSUMMARY
+grep average $TEST1RESULTS
